9f340c26f2b8ff4c755f9c804ebff1fd584e8b6f
leehyeeun <leehe0430@gmail.com>
2022-02-15 17:46:34 +0900

    2022/2/15 simple diary pjt call API

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,13 +1,36 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import LifeCycle from "./LifeCycle";
-import LifeCycleUnMount from "./LifeCycleUnMount";
+
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]); //일기 데이터 배열을 저장한다.
   const dataId = useRef(0);
+
+  const getData = async () => {
+    //API 호출하는 함수
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    //json 값들만 뽑아 온다.
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1, //0~1까지의 random 숫자
+        created_date: new Date().getTime(),
+        id: dataId.current++, //return을 바로 해버리니까 후위 연산자로 바로 더해버리기
+      };
+    });
+
+    setData(initData);
+    //20개 추출, map 함수를 사용하여 배열의 각 요소를 순회해서
+    //map 함수의 콜백함수에서 반환하는 값을 모아서 배열을 만들고 initData에 대입(return 하기 때문에 .map(() => {}) {} 사용)
+  };
+  useEffect(() => getData(), []); //mount할때의 life cycle
+
   const onCreate = (author, content, emotion) => {
     //새로운 일기를 추가하는 함수
     const created_date = new Date().getTime();
@@ -46,8 +69,6 @@ function App() {
   };
   return (
     <div className="App">
-      <LifeCycle />
-      <LifeCycleUnMount />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diarylst={data} onRemove={onRemove} onModify={onModify} />
     </div>
