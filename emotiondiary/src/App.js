9516a6a1498cc162181ec089491a87d0d9448d3d
leehyeeun <leehe0430@gmail.com>
2022-03-04 16:12:48 +0900

    2022/3/4 React winterload EmotionDiary pjt - Project 기초 공사 1 part

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -1,25 +1,58 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Pages
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import RouterTest from "./Components/RouterTest";
+
+//Components
+import MyButton from "./Components/MyButton";
+import MyHeader from "./Components/MyHeader";
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
       {" "}
       {/* 브라우저의 URL과 매핑을 위해 사용 */}
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
         <h2>App.js</h2>
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"default"}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouterTest />
       </div>
     </BrowserRouter>
   );
